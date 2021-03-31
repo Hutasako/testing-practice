99610ac8105e50d5c8d188355ee3ad6fdce28cf0
Hutasako <ejjungao@yahoo.com>
2021-03-31 11:27:56 -0400

    Part 1 completed

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,11 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(a, b=0) {
+  if (a) {
+		return a-b
+	}
+	return 0	
 }
 
 // ==================================================
@@ -20,16 +23,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(3,2,1)
+  if (result !== 1) throw new Error('Expected difference(3, 2, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1)
+  if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference()
+  if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,15 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(a, b=1) {
+    if (a){
+      if (!b){
+        // throw new Error('Error: Divide by 0')
+        return 'ERROR'
+      }
+      return a/b
+    }
+    return 0
 }
 
 // ==================================================
@@ -20,21 +27,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
+  var result = quotient(2,0);
+  if (result !== 'ERROR') throw new Error('Expected quotient(2, 0) to be ERROR. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = quotient(6, 2, 1);
+  if (result !== 3) throw new Error('Expected quotient(6, 2, 1) to be 3. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = quotient(6);
+  if (result !== 6) throw new Error('Expected quotient(6) to be 6. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
